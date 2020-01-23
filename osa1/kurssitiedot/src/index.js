b0f83cfba4b7ec45b712915e553a5d56c6975d5d
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2020-01-23 17:45:12 +0200

    Add 1.2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    );
+};
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>Number of exercises {props.total}</p>
+        </div>
+    );
+};
+
+const Part = (props) => {
+    return (
+        <p>{props.content.part} {props.content.exercises}</p>
+    );
+};
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part content={props.content[0]} />
+            <Part content={props.content[1]} />
+            <Part content={props.content[2]} />
+        </div>
+    );
+};
+
 const App = () => {
     const course = 'Half Stack application development';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
+    const content = [
+        {part: 'Fundamentals of React', exercises: 10},
+        {part: 'Using props to pass data', exercises: 7},
+        {part: 'State of a component', exercises: 14}
+    ];
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header course={course} />
+            <Content content={content} />
+            <Total total={content[0].exercises + content[1].exercises + content[2].exercises} />
         </div>
     );
 };
