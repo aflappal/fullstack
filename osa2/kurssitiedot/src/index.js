d7cad51c3c70d7c04724d74b5fd79ecd4711f7ef
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2020-01-31 17:23:56 +0200

    Add 2.5

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,36 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = (props) => {
-    return <h2>{props.course}</h2>;
-};
-
-const Total = ({parts}) => {
-    let timesum = parts.map(c => c.exercises).reduce((a, b) => a + b);
-    return <p><b>Total of {timesum} exercises</b></p>;
-};
-
-const Part = (props) => {
-    return <p>{props.part.name} {props.part.exercises}</p>;
-};
-
-const Content = (props) => {
-    return (
-        <div>
-            {props.parts.map(part => <Part key={part.id} part={part} />)}
-        </div>
-    );
-};
-
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    );
-};
+import Course from './components/Course';
 
 const App = () => {
     const courses = [
