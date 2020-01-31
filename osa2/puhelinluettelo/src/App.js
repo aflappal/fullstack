1fe7818f2f7e72eaf8840cab832ae3e2a098def6
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2020-01-31 19:40:17 +0200

    Add 2.7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -15,8 +15,13 @@ const App = () => {
 
     const addName = (event) => {
         event.preventDefault();
-        setPersons(persons.concat({name: newName}));
-        setNewName('');
+
+        if (persons.map(rec => rec.name).includes(newName)) {
+            alert(`${newName} is already added to the phonebook`);
+        } else {
+            setPersons(persons.concat({name: newName}));
+            setNewName('');
+        }
     };
 
     const handleRecordChange = (event) => {
