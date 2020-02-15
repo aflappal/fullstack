31c4290f13cdd1e3508b6d663f12525e63d40862
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2020-02-15 20:03:46 +0200

    Add 4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,15 +2,18 @@ const config = require('./utils/config');
 const express = require('express');
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const mongoose = require('mongoose');
 
 const app = express();
 
 const mongoUrl = config.MONGODB_URI;
+mongoose.set('useFindAndModify', false);
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
---
+++ osa4/blogilista/controllers/users.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
+
+usersRouter.get('/', async (req, res) => {
+    const users = await User.find({});
+    res.json(users.map(u => u.toJSON()));
+});
+
+usersRouter.post('/', async (req, res) => {
+    const body = req.body;
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const user = new User(
+        {
+            username: body.username,
+            name: body.name,
+            passwordHash
+        }
+    );
+
+    const addedUser = await user.save();
+    res.status(201).json(addedUser.toJSON());
+});
+
+usersRouter.delete('/:id', async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(204).end();
+    } catch (e) {
+        res.status(204).end();
+    }
+});
+
+usersRouter.put('/:id', async (req, res) => {
+    const user = req.body;
+    try {
+        const updated = await User.findByIdAndUpdate(req.params.id, user,  { new: true });
+        res.json(updated.toJSON());
+    } catch (e) {
+        res.status(404).end();
+    }
+});
+
+module.exports = usersRouter;
