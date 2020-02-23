3576248330df2e1fa4966e9d5afb06b728ec50f3
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2020-02-23 16:43:59 +0200

    Add 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 describe('<Blog />', () => {
@@ -23,11 +23,27 @@ describe('<Blog />', () => {
             <Blog blog={blog} />
         );
 
-        //expect(component.container.querySelector('.blogBasicInfo')).toBeDefined();
+        expect(component.container.querySelector('.blogBasicInfo')).toBeVisible();
+
         expect(component.container).toHaveTextContent('Test title');
         expect(component.container).toHaveTextContent('Mr. Testman');
 
-        expect(component.container.querySelector('.blogExtendedInfo'))
-            .toHaveStyle('display: none');
+        expect(component.container.querySelector('.blogExtendedInfo')).not.toBeVisible();
+    });
+
+    test('also renders url and likes when view clicked', () => {
+        const component = render(
+            <Blog blog={blog} />
+        );
+
+        const button = component.getByText('view');
+        fireEvent.click(button);
+
+        expect(component.container.querySelector('.blogBasicInfo')).toBeVisible();
+
+        expect(component.container).toHaveTextContent('Test title');
+        expect(component.container).toHaveTextContent('Mr. Testman');
+
+        expect(component.container.querySelector('.blogExtendedInfo')).toBeVisible();
     });
 });
