9160c5590481e1e3b65cf1cac1fecd52ccd076be
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2020-02-29 21:58:13 +0200

    Implement 6.3

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,15 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const oldAnecdote = state.find(a => a.id === id)
+      const newAnecdote = { ...oldAnecdote, votes: oldAnecdote.votes + 1}
+      return state.map(a => a.id !== id ? a : newAnecdote)
+    default:
+      return state
+  }
 }
 
 export default reducer
